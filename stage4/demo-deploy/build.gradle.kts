328e383e17fdb6248474a7b3d1e6b73f1d379cdf
Huang Jian-Min <yfr.huang@hotmail.com>
2020-09-13 16:40:16 +0800

    update swagger related

--- stage4/demo-deploy/build.gradle.kts
+++ stage4/demo-deploy/build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java")
